51a8c9c1741ea851eb5024c56e615d953243d113
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:56:48 +0200

    Update UserList.h

--- UserList.h
+++ UserList.h
@@ -1,22 +1,18 @@
 #pragma once
-#include <string>
 #include "User.h"
-
-using namespace std;
 class UserList
 {
 private:
-	User* users; //array
-	int capacity;
-	int usersCount;
-	int* ptr;
-
+	User* users; 
+	int capacity, usersCount;
 public:
+	int getUserCount();//Function returns userCount
+	UserList();//default constructor
 	UserList(int capacity);
-	void addUser(User user); // at the end of the array.
+	void addUser(User user); // at the end of the array
 	User& searchUser(string name);
 	User& searchUser(int id);
 	void deleteUser(int id);
-	friend ostream& operator<<(ostream& output, UserList& userList);//to display all books
+	friend ostream& operator<<(ostream& output, UserList& userList);//to display all users
 	~UserList();
 };
