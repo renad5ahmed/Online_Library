3d14be2e7e4c98e01f68eb594ef55d373a39a1aa
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:53:59 +0200

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,12 +1,38 @@
 #pragma once
-#include <string>
+#ifndef BOOK_H
+#define BOOK_H
+#include<cstring>
+#include<iostream>
+#include "User.h"
 using namespace std;
 class Book
 {
-	int id, position;
-	string name;
+private:
+	bool AuthorExist;
+	string title,isbn, category;
+	int id,noOfRatings;
+	double averageRating,sumOfRatings;
+	User author;
 public:
-	Book(int i, string n);
-	//static int cntBook;
+	void AuthorExists(bool u);
+	bool getAuthorExistOr()const;
+	void setAuthor(User user);
+	Book(); 
+	Book(string tit, string isb, string categ);
+	Book(const Book& book); 
+	string getTitle()const;
+	string getISBN()const;
+	int getId()const;
+	void setID(int id);
+	void setRate(double rating);
+	void setTitle(string name);
+	void setCategory(string cat);
+	string getCategory()const;
+	double getRate()const;
+	User getAuthor()const;
+	void rateBook(int rating);
+	bool operator==(const Book& book);
+	friend ostream& operator<<(ostream& output, const Book& book);
+	friend istream& operator>>(istream& input, Book& book);
 };
-//int Book::cntBook = 0;
+#endif // BOOK_H
