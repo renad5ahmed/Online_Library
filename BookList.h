cd13d120e47c08402f05e62badd38237bf0b0e7f
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:54:52 +0200

    Update BookList.h

--- BookList.h
+++ BookList.h
@@ -1,26 +1,22 @@
 #pragma once
-#include <string>
 #include "Book.h"
-#include "User.h"
-using namespace std;
-
+#include "UserList.h"
 class BookList
 {
 private:
 	Book* books;
-	int capacity;
-	int booksCount;
+	int capacity, booksCount;
 public:
-	BookList(const BookList& anotherList); //copy cons
+	BookList(const BookList& anotherList);//copy constructor
+	void addBook(Book book);
 	BookList(int capacity);
 	Book& searchBook(string name);
 	Book& searchBook(int id);
-	void deleteBook(int id); //delete a book
+	void deleteBook(int id); 
 	Book getTheHighestRatedBook();
-	Book* getBooksForUser(User); // get all books of this author
+	Book* getBooksForUser(User); 
 	Book& operator [ ] (int position);
 	friend ostream& operator<<(ostream& output, BookList& booklist); //to display all books
 	~BookList();
-
+	int getBooksCount();//Function returns booksCount
 };
-
