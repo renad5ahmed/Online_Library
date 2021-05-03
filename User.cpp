b9cb64ed3b911762779c567fc170d74876a65910
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:55:18 +0200

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,41 +1,85 @@
 #include "User.h"
-#include <string>
 #include <iostream>
-//User::User(int i, string n): id(i), name(n){}
-
-User::User()//default cons
+using namespace std;
+#include <string>
+#include "UserList.h"
+User::User() //default cons: set default values
 {
-	name = "";
-	password = "";
+	ID = 0;
 	age = 0;
+	name = "";
 	email = "";
-	id = 0;
-	cout << "USERS MENU" << '\n' << "1- create a USER and add it to the list" << '\n' << "2- search for a user" << '\n' << "3- Display all users" << '\n' << "4- Back to the main menu" << '\n';
+	password = "";
 }
-
-void User::addUser()
+void User::setPass(string pass)
 {
-	cout << "Enter the user information in this order" << '\n' << "Name Age Email Password (Space separated)" << '\n';
-
+	password = pass;
 }
-
-User::User(string name, int age, string email, string password)//parameterized cons
+bool User::operator==(const User& user)  // compares values
+{
+	if ((name == user.name) && (age = user.age) && (email == user.email) && (password == user.password))
+		return true;
+	else
+		return false;
+}
+string User::getName()const // name getter
+{
+	return name;
+}
+string User::getPassword()const // password getter
+{
+	return password;
+}
+string User::getEmail()const // email getter
+{
+	return email;
+}
+void User::setName(string name)
 {
 	this->name = name;
-	this->age = age;
-	this->email = email;
-	this->password = password;
-	id = cntUser;
-	User::cntUser++;
-
 }
-
-void User::printUser()
+int User::getID()const
 {
-	id = 1;
-	cout << "========User " << id << " info =========" << '\n';
-	cout << "Name: " << name << '\n' << "Age: " << age << '\n' << "Id: " << id << '\n' << "Email: " << email << '\n';
-	cout << "=============================" << '\n';
-	id++;
+	return ID;
 }
-
+void User::setID(int id) {
+	ID = id;
+}
+void User::setAge(int age) {
+	this->age = age;
+}
+int User::getAge()const // Age getter
+{
+	return age;
+}
+void User::setEmail(string email)
+{
+	this->email = email;
+}
+User::User(string na, int ag, string em, string pass)// parameterized cons
+{
+	name = na;
+	age = ag;
+	email = em;
+	password = pass;
+}
+User::User(const User& user) //copy cons: copy values
+{
+	name = user.name;
+	age = user.age;
+	email = user.email;
+	password = user.password;
+	ID = user.ID;
+}
+istream& operator>>(istream& is, User& user1)  // istream operator extraction for input
+{
+	cout << "Enter the user information in this order" << '\n' << "Name Age Email Password (Space separated)" << '\n';
+	is >> user1.name >> user1.age >> user1.email >> user1.password;
+	return is;
+}
+ostream& operator<<(ostream& os, const User& user) { //ostream operator insertion for output
+	os << "========User " << (user.ID) << " info =========" << '\n';
+	os << "Name: " << user.name << '\n' << "Age: " << user.age << '\n' << "Id: " << user.ID << '\n' << "Email: " << user.email << '\n';
+	os << "=============================" << '\n';
+	return os;
+}
