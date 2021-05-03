7220fa36dac8e7d55820cb132fb08453a3edd0cb
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:56:15 +0200

    Update UserList.cpp

--- UserList.cpp
+++ UserList.cpp
@@ -1,17 +1,65 @@
 #include "UserList.h"
-#include "BookList.h"
-#include <iostream>
-using namespace std;
-UserList::UserList(int capacity)
+#include "User.h"
+int UserList::getUserCount()//Returns userCount
+{
+	return usersCount;
+}
+UserList::UserList()//default constructor
+{
+	capacity = 0;
+	usersCount = 0;
+}
+UserList::UserList(int capacity)//Setting the dynamic array's size
 {
 	this->capacity = capacity;
+	this->users = new User[capacity];
+	this->usersCount = 0;
 }
-
-void UserList::deleteUser(int id)//deletetUser(2)
+void UserList::addUser(User user)//Adding function
 {
-	for (int i = id; i < capacity; i++) users[i] = users[i + 1];
-	//users[capacity] = '\0';//y3ny mafeesh akher element
+	if (this->usersCount < this->capacity) {
+		user.setID(this->usersCount+1);
+		this->users[this->usersCount] = user;
+		this->usersCount++;
+	}
 }
-
-//ostream& operator<<(ostream& output, UserList& userList){for (int i = 0; i < 5; i++)
-		//output << (*UserList::ptr).users[i] << endl;   return output;}
+User& UserList::searchUser(string name)//Searching by name method
+{
+	for (int i = 0; i < usersCount; i++) {
+		if (users[i].getName() == name) 
+			return users[i];
+	}   
+}
+User& UserList::searchUser(int id)//Searching by ID method
+{
+	for (int i = 0; i < capacity; i++) {
+		if ((users[i]).getID() == id) 
+			return users[i];
+	}
+}
+void UserList::deleteUser(int id)//delete User
+{
+	for (int i = 0; i < capacity; i++)
+	{
+		if ((users[i]).getID() == id)
+		{
+			while (i < usersCount )
+			{
+				users[i] = users[i + 1];
+				i++;
+			}
+			usersCount--;
+			break;
+		}
+	}
+}
+ostream& operator<<(ostream& output, UserList& userList)//Ostream operator insertion for output 
+{
+	for ( int i = 0; i < userList.usersCount; i++) {		
+		output << userList.users[i] << '\n';
+	}
+    return output;
+}
+UserList:: ~UserList() {//destructor
+	delete[] users;
+}
