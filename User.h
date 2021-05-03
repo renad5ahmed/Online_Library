00fb90148a0316038b885b41eea553cd4564ff40
Renad Ahmed <renadahmed581@gmail.com>
2021-05-03 23:55:45 +0200

    Update User.h

--- User.h
+++ User.h
@@ -1,16 +1,30 @@
 #pragma once
-#include <string>
+#ifndef USER_H
+#define USER_H
+#include<cstring>
+#include<iostream>
 using namespace std;
 class User
 {
-	int id, age;
-	string name, password, email;
+private:
+	int age, ID;
+	string password, email, name;
 public:
-	//User(int i, string n);
-	User();
-	void addUser();
-	//~User();
-	User(string name, int age, string email, string password);
-	static int cntUser;
-	void printUser();
+	void setID(int id);
+	User(); 
+	User(string na, int ag, string em, string pass);
+	User(const User& user);
+	bool operator==(const User& user);
+	void setAge(int age);
+	void setName(string name);
+	void setPass(string pass);
+	void setEmail(string email);
+	string getName() const;
+	string getPassword()const;
+	string getEmail()const;
+	int getID()const;
+	int getAge()const;
+	friend istream& operator >>(istream& input, User& user);
+	friend ostream& operator<<(ostream& os, const User& user);
 };
+#endif // USER_H
